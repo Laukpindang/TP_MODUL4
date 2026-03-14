fac04ac928a884f660b253de264c6e4575881584
Laukpindang <ferdiansyahbandung3@gmail.com>
2026-03-14 15:24:34 +0700

    second commit

---
+++ TP_MODUL4_103022400053/KodePos.cs
@@ -0,0 +1,28 @@
+﻿namespace TP_MODUL4_103022400053
+{
+    internal class KodePos
+    {
+        public static string getKodePos(string kelurahan) 
+        {
+            string[] listKelurahan = { "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura", "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer" };
+            string[] kodepos = { "40266", "40287", "40267", "40256", "40287", "40286", "40286", "40286", "40272", "40274" };
+            int maxIdx = kodepos.Length - 1;
+
+            string current = "40274";
+            int level = 0;
+
+            while ((current == "40274") && (level <= maxIdx))
+            {
+                if (kelurahan == kodepos[level])
+                {
+                    current = kodepos[level];
+                }
+                else
+                {
+                    level++;
+                }
+            }
+            return current;
+        }
+    }
+}
--- TP_MODUL4_103022400053/Program.cs
+++ TP_MODUL4_103022400053/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TP_MODUL4_103022400053;
+
+Console.WriteLine("Batununggal: " + KodePos.getKodePos("Batununggal"));
