ba96191c017d13dba29de3ec38aa68754f6ba865
Laukpindang <ferdiansyahbandung3@gmail.com>
2026-03-14 15:38:44 +0700

    menambahkan class DoorMachine dan KodePos

---
+++ TP_MODUL4_103022400053/DoorMachine.cs
@@ -0,0 +1,49 @@
+﻿namespace TP_MODUL4_103022400053
+{
+    internal class DoorMachine
+    {
+        public enum State { Terkunci, Terbuka }
+        public enum Trigger { KunciPintu, BukaPintu };
+        public State currentState = State.Terkunci;
+        public DoorMachine()
+        {
+            Console.WriteLine("Pintu " + currentState);
+        }
+        public void UbahState(Trigger trigger)
+        {
+            switch (currentState)
+            {
+                case State.Terkunci:
+                    if (trigger == Trigger.BukaPintu)
+                    {
+                        currentState = State.Terbuka;
+                    }
+                    else if (trigger == Trigger.KunciPintu)
+                    {
+                        currentState = State.Terkunci;
+                    }
+                    break;
+
+                case State.Terbuka:
+                    if (trigger == Trigger.KunciPintu)
+                    {
+                        currentState = State.Terkunci;
+                    }
+                    else if (trigger == Trigger.BukaPintu)
+                    {
+                        currentState = State.Terbuka;
+                    }
+                    break;
+            }
+
+            if (currentState == State.Terkunci)
+            {
+                Console.WriteLine("Pintu terkunci");
+            }
+            else if (currentState == State.Terbuka)
+            {
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+        }
+    }
+}
--- TP_MODUL4_103022400053/Program.cs
+++ TP_MODUL4_103022400053/Program.cs
@@ -1,3 +1,9 @@
 ﻿using TP_MODUL4_103022400053;
 
 Console.WriteLine("Batununggal: " + KodePos.getKodePos("Batununggal"));
+
+DoorMachine doorMachine = new DoorMachine();
+Console.WriteLine("Trigger: BukaPintu");
+doorMachine.UbahState(DoorMachine.Trigger.BukaPintu);
+Console.WriteLine("Trigger: KunciPintu");
+doorMachine.UbahState(DoorMachine.Trigger.KunciPintu);
